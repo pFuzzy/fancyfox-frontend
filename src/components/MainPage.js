f751ba004101cb7e808ef91364c3d44279009cd5
pFuzzy <p.fazii888@gmail.com>
2021-01-13 11:02:10 +0100

    complete main page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { AddButton, Header, Logo } from '../styled-components/Header';
 import FancyFox from '../images/FancyFox.png';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Content, Post, Video } from '../styled-components/Content';
 
 const MainPage = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8762/media/all')
+      .then((res) => setVideos(res.data));
+  }, []);
   return (
     <div>
       <Header>
         <Logo src={FancyFox} alt='FancyFoxLogo' />
         <AddButton to='/add'>Add post</AddButton>
       </Header>
+      <Content>
+        {videos.map((video) => {
+          return (
+            <Post>
+              <div>{video.title}</div>
+              <Video url={video.url} />
+            </Post>
+          );
+        })}
+      </Content>
     </div>
   );
 };
---
+++ src/styled-components/Content.js
@@ -0,0 +1,16 @@
+import ReactPlayer from 'react-player';
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  width: 100vw;
+  height: auto;
+  position: absolute;
+  top: 8vh;
+`;
+
+export const Post = styled.div`
+  width: 35vw;
+  margin: auto;
+`;
+
+export const Video = styled(ReactPlayer)``;
