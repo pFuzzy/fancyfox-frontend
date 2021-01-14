fe72b962cb07e96c0e92b9f886fe20fa52ce3111
pFuzzy <p.fazii888@gmail.com>
2021-01-14 14:02:28 +0100

    lower video volume

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import {
   Title,
   CommentContainer,
@@ -8,50 +8,55 @@ import {
   CommentForm,
   CommentFormTextArea,
   CommentFormButton,
-  CommentLabel
-} from "../styled-components/DetailPageStyle";
-import {Content, Post, Video, Rating, Upvote, Downvote} from "../styled-components/ContentStyle";
-import config from "../config.json";
+  CommentLabel,
+} from '../styled-components/DetailPageStyle';
+import {
+  Content,
+  Post,
+  Video,
+  Rating,
+  Upvote,
+  Downvote,
+} from '../styled-components/ContentStyle';
+import config from '../config.json';
 
 const DetailPage = () => {
   const [video, setVideo] = useState({});
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
-  const [status, setStatus ]= useState("");
+  const [status, setStatus] = useState('');
 
   useEffect(() => {
     setIsLoading(true);
-    axios.get(config.API +  "/media/" + id)
-    .then((res) => {
+    axios.get(config.API + '/media/' + id).then((res) => {
       setVideo(res.data);
       setIsLoading(false);
     });
   }, [id, status]);
-
 
   function addComment(event) {
     event.preventDefault();
     let comment = {
       comment: event.target[0].value,
       mediaId: id,
-    }
-    axios.post(config.API +"/media/add-comment", comment)
-      .then((res) => {
-        setStatus(res.data);
-        setStatus("");
-      })
+    };
+    axios.post(config.API + '/media/add-comment', comment).then((res) => {
+      setStatus(res.data);
+      setStatus('');
+    });
   }
 
   function upvote(event) {
     event.preventDefault();
-    axios.put(config.API + "/media/increase-rating", {videoId: id}).then((res) => {
-      let ratingField = document.querySelector("#rating");
-      if (res.data === "Upvote successful") {
-        ratingField.innerText = parseInt(ratingField.innerText) + 1;
-      }
-    })
+    axios
+      .put(config.API + '/media/increase-rating', { videoId: id })
+      .then((res) => {
+        let ratingField = document.querySelector('#rating');
+        if (res.data === 'Upvote successful') {
+          ratingField.innerText = parseInt(ratingField.innerText) + 1;
+        }
+      });
   }
-
 
   if (isLoading) {
     return <></>;
@@ -60,20 +65,21 @@ const DetailPage = () => {
       <Content>
         <Post>
           <Title>{video.title}</Title>
-          <Video url={video.url} />
-          <Upvote onClick={upvote}/>
-          <Downvote onClick={upvote}/>
-          <Rating id="rating">{video.rating}</Rating>
+          <Video volume={0.1} url={video.url} />
+          <Upvote onClick={upvote} />
+          <Downvote onClick={upvote} />
+          <Rating id='rating'>{video.rating}</Rating>
         </Post>
         <CommentContainer>
-          <CommentForm  onSubmit={addComment}>
+          <CommentForm onSubmit={addComment}>
             <CommentLabel>Add New Comment</CommentLabel>
-            <CommentFormTextArea></CommentFormTextArea><br/>
-            <CommentFormButton type="submit">submit</CommentFormButton>
+            <CommentFormTextArea></CommentFormTextArea>
+            <br />
+            <CommentFormButton type='submit'>submit</CommentFormButton>
           </CommentForm>
-            {video.comments.map((comment) => {
-              return <Comment key={comment.id}>{comment}</Comment>;
-            })}
+          {video.comments.map((comment) => {
+            return <Comment key={comment.id}>{comment}</Comment>;
+          })}
         </CommentContainer>
       </Content>
     );
